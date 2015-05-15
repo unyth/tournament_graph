37f46d30894a1640049242444d17c8276ec3d702
Yuan Cao <yuan.cao.email@gmail.com>
2015-05-15 17:17:55 -0400

    Simplify + add test

--- GoMatch.py
+++ GoMatch.py
@@ -24,6 +24,9 @@ from random import choice
 from GoPlayer import Player
 import unittest
 
+class AlteringResultErrorion(Exception):
+    pass
+
 class Match(object):
     """This is the Match class.
 
@@ -38,6 +41,8 @@ class Match(object):
         assert (isinstance(player_1, Player) and 
                 isinstance(player_2, Player))
 
+        self.match_type = match_type
+
         if match_type != "open":
             if player_1.rating > player_2.rating:
                 self.w_player = player_1
@@ -45,28 +50,39 @@ class Match(object):
             else:
                 self.w_player = player_2
                 self.b_player = player_1
+            
+            self.handi = self.w_player.rating - self.b_player.rating
+
         else:
             players = [player_1, player_2]
 
             self.w_player = choice(players)
-            players.pop(self.w_player)
+            players.remove(self.w_player)
             self.b_player = players[0]
-
-        self.match_type = match_type
-
-        if match_type != "open":
-            self.handi = player.w_player.rating - player.b_player.rating
+            
+            self.handi = 0
 
         if self.handi != 0:
             self.komi = 0.5
+        else:
+            self.handi = 7.5
         
         self.result = "unplayed"
     
     def update_result(self, result):
+        """The proper way to update the game result
+        
+        The game can only have one result, will raise exception when
+        trying to change more than once.
+        """
         if self.result == "unplayed" and result != "unplayed":
             self.result = result
+        else:
+            raise AlteringResultErrorion
 
     def set_result(self, result):
+        """When the result must be changed, due to initial error."""
+
         self.result = result
 
 
@@ -77,10 +93,10 @@ class MatchTestCase(unittest.TestCase):
         self.player_list = sorted(player_list)
 
     def test_player_assignment_handi(self):
-        self.match = Match(player_list[0], player_list[1],
+        self.match = Match(self.player_list[0], self.player_list[1],
                            match_type="handi")
-        self.assertTrue(self.match.w_player == player_list[1] and
-                        self.match.b_player == player_list[0])
+        self.assertTrue(self.match.w_player == self.player_list[1] and
+                        self.match.b_player == self.player_list[0])
 
     def test_player_assignment_open(self):
         as_black = 0
@@ -94,6 +110,21 @@ class MatchTestCase(unittest.TestCase):
                 as_black += 1
         self.assertTrue(as_black > 0)
         self.assertTrue(as_white > 0)
+
+    def test_result_assignment(self):
+        self.match = Match(player_list[4], player_list[5])
+        a_result = "White win by resignation"
+        self.match.set_result(a_result)
+        self.assertTrue(self.match.result == a_result)
+
+    def test_update_result(self):
+        self.match = Match(player_list[6], player_list[7])
+        a_result = "White win by resignation"
+        self.match.update_result(a_result)
+        another_result = "Black win by 4.5 pt"
+        with self.assertRaises(AlteringResultErrorion):
+            self.match.update_result(another_result)
+
 
 if __name__ == "__main__":
     unittest.main()
